3de4c3c4c7a7d1d3398c02d117c608ea656eb52c
MyeongchanJin <jmcchan@naver.com>
2020-05-22 18:40:17 +0900

    2588번 문제

---
+++ src/beakjoon/n02588/Main.java
@@ -0,0 +1,21 @@
+package beakjoon.n02588;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int b100, b10, b1;
+        b100 = b / 100;
+        b10 = (b % 100) / 10;
+        b1 = (b % 100) % 10;
+
+        System.out.println(b1 * a);
+        System.out.println(b10 * a);
+        System.out.println(b100 * a);
+        System.out.println(a * b);
+
+        s.close();
+    }
+}
