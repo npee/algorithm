0712ecb00959d1c8b07cc6823493152c4b9e0607
MyeongchanJin <jmcchan@naver.com>
2020-05-22 18:42:48 +0900

    2741번 문제

---
+++ src/beakjoon/n02741/Main.java
@@ -0,0 +1,17 @@
+package beakjoon.n02741;
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = s.nextInt();
+        for (int i = 1; i <= count; i++) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
+        s.close();
+    }
+}
