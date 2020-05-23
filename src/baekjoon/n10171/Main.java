40b9d13fd15bff8e2a83105e83372e32873c0502
MyeongchanJin <jmcchan@naver.com>
2020-05-24 04:20:48 +0900

    10171번 문제

---
+++ src/baekjoon/n10171/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.n10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\\    /\\\n");
+        System.out.print(" )  ( \')\n");
+        System.out.print("(  /  )\n");
+        System.out.print(" \\(__)|");
+    }
+
+}
