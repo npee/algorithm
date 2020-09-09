47ec1e49704c223423e13919d885fd389de7b970
MyeongchanJin <jmcchan@naver.com>
2020-09-10 03:10:04 +0900

    100%

--- src/programmers/kakao/blind_recruitment_2020/_4_lyrics_search/Solution.java
+++ src/programmers/kakao/blind_recruitment_2020/_4_lyrics_search/Solution.java
@@ -2,30 +2,38 @@ package programmers.kakao.blind_recruitment_2020._4_lyrics_search;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Solution {
     public static void main(String[] args) {
         String[] words = {"frodo", "front", "frost", "frozen", "frame", "kakao", "a"};
-        String[] quries = {"fro??", "????o", "fr???", "fro???", "pro?", "?", "frodo"};
+        String[] quries = {"fro??", "????o", "fr???", "fro???", "pro?", "?", "frodo", "?????"};
 
         print(solution(words, quries));
     }
     public static int[] solution(String[] words, String[] queries) {
         int[] results = new int[queries.length];
-        Trie trie = new Trie();
+        Trie[] tries = new Trie[100001];
 
-        trie.printTrie();
+        for (String word: words) {
+            int i = word.length();
 
-        for (int i = 0; i < words.length; i++) {
-            trie.insert(words[i]);
+            if (tries[i] == null) {
+                tries[i] = new Trie();
+            }
+
+            tries[i].insert(word);
         }
 
         for (int i = 0; i < queries.length; i++) {
-            if (trie.contains(queries[i])) {
-                results[i]++;
+            int j = queries[i].length();
+
+            if (tries[j] == null) {
+                results[i] = 0;
+            } else {
+                results[i] = tries[j].getCount(queries[i]);
             }
         }
+
         return results;
     }
 
@@ -83,85 +91,104 @@ public class Solution {
 }
 
 class TrieNode {
-    private Map<Character, TrieNode> children = new HashMap<>();
-    private boolean end;
+    private Map<Character, TrieNode> children;
+    private int count;
+
+    TrieNode() {
+        this.children = new HashMap<>();
+        this.count = 0;
+    }
 
     Map<Character, TrieNode> getChildren() {
         return this.children;
     }
-    boolean getEnd() {
-        return this.end;
+
+    public int getCount() {
+        return count;
     }
 
-    void setEnd(boolean end) {
-        this.end = end;
+    public void increaseCount() {
+        this.count++;
     }
-
-
 }
 
 class Trie {
-    private TrieNode root;
-    private int quantityOFWord;
+    public TrieNode root;
+    public TrieNode reverseRoot;
+    public int wordCount;
 
     Trie() {
+        wordCount = 0;
         root = new TrieNode();
+        reverseRoot = new TrieNode();
     }
 
     void insert(String word) {
-        TrieNode currentNode = this.root;
+        commonInsert(word);
+        reverseInsert(word);
+    }
+
+    void commonInsert(String word) {
+        TrieNode currentNode = root;
 
         for (int i = 0; i < word.length(); i++) {
             currentNode = currentNode.getChildren().computeIfAbsent(word.charAt(i), ch -> new TrieNode());
+            currentNode.increaseCount();
         }
-        quantityOFWord++;
-        currentNode.setEnd(true);
     }
 
-    boolean contains(String query) {
-        TrieNode currentNode = this.root;
-        System.out.println(query);
+    void reverseInsert(String word) {
+        TrieNode currentNode = reverseRoot;
 
-        int wordLength = 0;
+        for (int i = word.length() - 1; i >= 0; i--) {
+            currentNode = currentNode.getChildren().computeIfAbsent(word.charAt(i), ch -> new TrieNode());
+            currentNode.increaseCount();
+        }
+        this.wordCount++;
+    }
+
+    int getCount(String query) {
+        if (query.charAt(0) == '?') {
+            return getCountInReverseCase(query);
+        } else {
+            return getCountInCommonCase(query);
+        }
+    }
+
+    int getCountInCommonCase(String query) {
+        TrieNode currentNode = this.root;
 
         for (int i = 0; i < query.length(); i++) {
-            char queryChar = query.charAt(i);
-
-            if (queryChar == '?') {
-                wordLength++;
-                continue;
+            char ch = query.charAt(i);
+            if (ch == '?') {
+                break;
             }
-
-            TrieNode findNode = currentNode.getChildren().get(queryChar);
-            System.out.print(queryChar);
-
-            if (findNode == null) {
-                System.out.println();
-                return false;
+            if (!currentNode.getChildren().containsKey(ch)) {
+                return 0;
             }
-
-            currentNode = findNode;
-            wordLength++;
+            currentNode = currentNode.getChildren().get(ch);
         }
-
-        if (wordLength != query.length()) {
-            return false;
-        }
-
-        System.out.println();
-        System.out.println("query length: " + query.length());
-        System.out.println("word length: " + wordLength);
-
-        return currentNode.getEnd();
+        return currentNode.getCount();
     }
 
-    void printTrie() {
-        TrieNode nodePointer = this.root;
-        AtomicReference<String> temp = new AtomicReference<>("");
-        for (char i = 'a'; i < 'z'; i++) {
+    int getCountInReverseCase(String query) {
+        TrieNode currentNode = this.reverseRoot;
 
+        for (int i = query.length() - 1; i >= 0; i--) {
+            if (query.charAt(query.length() - 1) == '?') {
+                return wordCount;
+            }
+
+            char ch = query.charAt(i);
+
+            if (ch == '?') {
+                break;
+            }
+            if (!currentNode.getChildren().containsKey(ch)) {
+                return 0;
+            }
+            currentNode = currentNode.getChildren().get(ch);
         }
-        System.out.println();
+        return currentNode.getCount();
     }
-
 }
