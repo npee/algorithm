d892e00d117dcbadea733d23361eac9a81cf4974
MyeongchanJin <jmcchan@naver.com>
2020-09-19 23:43:04 +0900

    가장 큰 수

--- src/programmers/ex/sort02/Solution.java
+++ src/programmers/ex/sort02/Solution.java
@@ -1,4 +1,20 @@
 package programmers.ex.sort02;
 
 public class Solution {
+    public static void main(String[] args) {
+        int[] numbers1 = {6, 10, 2};
+        int[] numbers2 = {3, 30, 34, 5, 9};
+    }
+
+    public static String solution(int[] numbers) {
+        String answer = "";
+
+        
+
+        return answer;
+    }
+
+    public static void merge_sort(int[] src, int from, int to) {
+
+    }
 }
