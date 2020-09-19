3d37ad88452d942af969464e5127334322940199
MyeongchanJin <jmcchan@naver.com>
2020-09-20 06:04:19 +0900

    가장 큰 수 정답

--- src/programmers/ex/sort02/Solution.java
+++ src/programmers/ex/sort02/Solution.java
@@ -1,20 +1,35 @@
 package programmers.ex.sort02;
 
+import java.util.*;
+
 public class Solution {
     public static void main(String[] args) {
         int[] numbers1 = {6, 10, 2};
         int[] numbers2 = {3, 30, 34, 5, 9};
+
+        System.out.println(solution(numbers1));
+        System.out.println(solution(numbers2));
+
     }
 
     public static String solution(int[] numbers) {
         String answer = "";
 
-        
+        PriorityQueue<String> queue = new PriorityQueue<>((str1, str2) -> Integer.parseInt(str2 + str1) - Integer.parseInt(str1 + str2));
 
-        return answer;
-    }
+        for (int number: numbers) {
+            queue.offer(String.valueOf(number));
+        }
 
-    public static void merge_sort(int[] src, int from, int to) {
+        while (!queue.isEmpty()) {
+            answer += queue.poll();
+        }
 
+        // 결과가 0인 경우
+        if (answer.charAt(0) == '0') {
+            return "0";
+        } else {
+            return answer;
+        }
     }
 }
